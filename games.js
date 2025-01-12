b514f8baf1f4c668b0b551f4c383607324d02580
kors-ak <alinakorsak86@gmail.com>
2025-01-13 00:01:06 +0300

    script for arithmetics is done

--- games.js
+++ games.js
@@ -1,4 +1,4 @@
-// Генератор случайных цветов
+//1 - Генератор случайных цветов
 
 function guessNumberGame() {
   let randomNumber = Math.floor(Math.random() * 100 + 1);
@@ -19,3 +19,65 @@ function guessNumberGame() {
     }
   }
 }
+
+//2 - Простая арифметика
+
+function arithmeticGame() {
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  let playAgain = true;
+  let greetingArray = ['Всё верно!', 'В яблочко!', 'Гениально!', 'У тебя великолепно получается!', 'Как тебе это удается? Решено отлично!', 'Именно так!', 'Идеальное решение!'];
+  const operationArray = ['+', '-', '*', '/'];
+
+  while (playAgain) {
+    let greeting = greetingArray[Math.floor(Math.random() * greetingArray.length)];
+    let operation = operationArray[Math.floor(Math.random() * operationArray.length)];
+    let a;
+    let b;
+    let answer;
+
+    switch (operation) {
+      case '+':
+        a = getRandomNumber(1, 30);
+        b = getRandomNumber(1, 30);
+        answer = a + b;
+        break;
+      case '-':
+        a = getRandomNumber(1, 30);
+        b = getRandomNumber(1, a);
+        answer = a - b;
+        break;
+      case '*':
+        a = getRandomNumber(1, 12);
+        b = getRandomNumber(0, 12);
+        answer = a * b;
+        break;
+      case '/':
+        b = getRandomNumber(1, 10);
+        answer = getRandomNumber(1, 10);
+        a = b * answer;
+        break;
+    }
+
+    let userAnswer = prompt(`Попробуй решить такую задачу: \n${a} ${operation} ${b} = ?`);
+
+    while (true) {
+      if (userAnswer == null) {
+        playAgain = false;
+        break;
+      } else if (userAnswer == answer) {
+        playAgain = confirm(`${greeting} Продолжаем?`);
+        break;
+      } else {
+        userAnswer = prompt(`Вариант хороший, но неверный. Попробуй решить задачу ещё раз: \n ${a} ${operation} ${b} = ?`);
+      }
+    }
+  }
+
+  if (playAgain === false) {
+    alert('Ждем тебя снова!')
+  }
+}
+
